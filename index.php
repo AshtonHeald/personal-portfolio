80fa9d4dd10d15454e8488a14744d11b2ccab0af
Ashton Heald <devtheash@gmail.com>
2022-12-16 15:37:25 -0600

    added no index, no follow for old portfolio

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="noindex, nofollow" />
 	<title>Ashton Heald - Web Portfolio</title>
 	<meta name="description" content="I'm Ash, a front-end web developer, an introverted writer, and an abstract thinker. This is my web portfolio!">
 	<meta name="keywords" content="Ashton, Heald, front-end, web dev">
