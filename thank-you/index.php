b32228f36707c88b2f8081cebfc057ddf20cfd73
Ashton Heald <devtheash@gmail.com>
2023-09-15 15:30:17 -0500

    edited thank-you page title

--- thank-you/index.php
+++ thank-you/index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ashton Heald - Thanks!</title>
+	<title>Ashton Heald | Thanks!</title>
 	<link rel="stylesheet" href="../assets/fontawesome/css/all.min.css">
 	<link rel="stylesheet" href="../assets/css/main.css">
 	<link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
